b969a455f1c290dacbcae9fab3d83461f8c9d459
vipksmosar <admin@npfx.ru>
2019-12-17 13:13:19 +0400

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -8,7 +8,7 @@ __status__ = "Sigma-test"
 import time
 import os
 import sys
-import subprocess
+
 from tqdm import tqdm
 import pandas as pd
 import threading
@@ -16,7 +16,7 @@ import socket
 import logging
 import logging.config
 import argparse
-
+import subprocess
 from support.import_file import import_file
 from support.ip_range import create_IP_pool
 
@@ -25,20 +25,21 @@ path=sys.path[0]
 
 arg_parser = argparse.ArgumentParser(description='ports_scanner_by_IP_from_xlsx_file')
 arg_parser.add_argument('--filein', type=str, help="--filein 'C:/CSV/srv_vm2.xlsx'")
-arg_parser.add_argument('--fileout', type=str, help="--fileout 'C:/CSV/HOSTS_VIRT_withScanned.csv'")
+arg_parser.add_argument('--fileout', type=str, help="--fileout 'C:/CSV/HOSTS_VIRT_withScanned.csv'", default='out_file.csv')
 arg_parser.add_argument('--ports',nargs='+', type=int, help="--ports 20 21 22 139 445 80 44")
-arg_parser.add_argument('--ports_range',nargs='+', type=int, help="--ports_range 20 25")
-arg_parser.add_argument('--ip_range', type=str, help="--ip_range 192.168.10.23-192.168.10.25")
-arg_parser.add_argument('--IPcol', type=str, help="--IPcol 'IP' - default 'IP'")
-arg_parser.add_argument('--OneHost', type=bool, help="--OneHost True, default False")
-arg_parser.add_argument('--Ping', type=bool, help="--Ping True, default False")
+arg_parser.add_argument('--ports_range',nargs='+', type=int, help="--ports_range 20 25", default=None)
+arg_parser.add_argument('--ip_range', type=str, help="--ip_range 192.168.10.23-192.168.10.25", default=None)
+arg_parser.add_argument('--IPcol', type=str, help="--IPcol 'IP' - default 'IP'", default='IP')
+arg_parser.add_argument('--OneHost', type=bool, help="--OneHost True, default False", default=False)
+arg_parser.add_argument('--Ping', type=bool, help="--Ping True, default False", default=False)
+arg_parser.add_argument('--DNS', type=bool, help="--DNS True, default False", default=False)
 args = arg_parser.parse_args()
 
 logging.config.fileConfig(os.path.join(path,'logger.conf'), defaults={'logfilename': os.path.join(path,'portscan.log')})
 logger = logging.getLogger('scanner')
 
 class scanner:
-    def __init__(self,  port_list, file_in, file_out, IP_col, flag_many_host=False, port_range=None, ip_range=None, flag_ping=False):
+    def __init__(self,  port_list, file_in, file_out, IP_col, flag_many_host, port_range, ip_range, flag_ping, flag_dns):
         self.IP_col = IP_col
         self.port_list = port_list
         self.file_in = file_in
@@ -47,16 +48,12 @@ class scanner:
         self.flag_ping = flag_ping
         self.ip_range = ip_range
         self.port_range = port_range
-        if self.file_out == None:
-            self.file_out = 'out_file.csv'
-        if self.IP_col == None:
-            self.IP_col = 'IP'
-
+        self.flag_dns = flag_dns
     
     def __import_file (self):
         file = import_file(self.file_in)
         DF = file.read()
-        IP_list = DF[self.IP_col].to_list()
+        IP_list = DF[self.IP_col].tolist()
         return IP_list
     
     def __ip_range (self):
@@ -84,15 +81,16 @@ class scanner:
             else:
                 ports = self.port_list
             print(self.file_out)
-            tcp_ports = scann_ports(IP, ports, self.file_out)
+            tcp_ports = scann_ports(IP, ports, self.file_out, self.flag_dns)
             tcp_ports.start_scann()
 
 class scann_ports(scanner):
     
-    def __init__ (self, IP, ports, file_out):
+    def __init__ (self, IP, ports, file_out, flag_dns):
         self.IP = IP
         self.ports = ports        
         self.file_out = file_out
+        self.flag_dns = flag_dns
         
     def __to_IP (self, ip):
         try:
@@ -105,10 +103,11 @@ class scann_ports(scanner):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(0.5)
         try:
-            try:
-                ip_host = socket.gethostbyname(ip_host)
-            except Exception as E:
-                None
+            #try:
+            #    ip_host = socket.gethostbyname(ip_host)
+            #except Exception as E:
+            #    self.list_of_scanned_hosts_not_resolved.append(ip_host)
+            #    return None
             connection = s.connect((ip_host, port_to_scan))
             self.list_of_scanned_hosts.append(ip_host)
         except:
@@ -117,14 +116,24 @@ class scann_ports(scanner):
     def __to_file (self, list_of_hosts, list_of_hosts_false, port):
         Data_hosts = pd.DataFrame(columns=[])
         Data_hosts_false = pd.DataFrame(columns=[])
+        logger.info('create result DF: acess '+ str(len(list_of_hosts)) + ' , denied ' + str(len(list_of_hosts_false)))
         Data_hosts['IP'] = list(set(list_of_hosts))
-        Data_hosts['Name']=Data_hosts['IP'].apply(self.__to_IP)
+        if self.flag_dns:
+            logger.info('start to dns')
+            Data_hosts['Name']=Data_hosts['IP'].apply(self.__to_IP)
+            
         Data_hosts['port_number'] = port
         Data_hosts['result'] = 'ACESS'
         Data_hosts_false['IP'] = list(set(list_of_hosts_false))
-        Data_hosts_false['Name']=Data_hosts_false['IP'].apply(self.__to_IP)
+        if self.flag_dns:
+            logger.info('start to dns')
+            Data_hosts_false['Name']=Data_hosts_false['IP'].apply(self.__to_IP)
         Data_hosts_false['port_number'] = port
         Data_hosts_false['result'] = 'DENIED'
+        logger.info('start out DF')
+        
+        self.list_of_scanned_hosts_not_resolved
+        
         DF = pd.concat([Data_hosts_false, Data_hosts])
         if os.path.isfile(self.file_out)==True:
             DF.to_csv(self.file_out, mode='a', sep='\t', encoding='utf-16', header=False, index=False)
@@ -138,17 +147,19 @@ class scann_ports(scanner):
         for port in self.ports:
             self.list_of_scanned_hosts = []
             self.list_of_scanned_hosts_false = []
+            self.list_of_scanned_hosts_not_resolved = []
             if type(self.IP) == list:
                 for i in tqdm(self.IP):
                     thread_port = threading.Thread(target=self.__scan_hosts_ports, kwargs={'ip_host': i, 'port_to_scan': port})
                     thread_port.start()
-                    while threading.active_count()>700 :
+                    while threading.active_count()>900 :
                         time.sleep(0.5)
             else:
                 thread_port = threading.Thread(target=self.__scan_hosts_ports, kwargs={'ip_host': self.IP, 'port_to_scan': port})
                 thread_port.start()  
+                thread_port.join()
             while thread_port.isAlive() == True:
-                time.sleep(15)
+                time.sleep(1)
             self.__to_file( self.list_of_scanned_hosts, self.list_of_scanned_hosts_false, port)
         logger.info('SCAN IS OVER')
         
@@ -211,5 +222,5 @@ class scann_icmp(scanner):
 
         
     
-scan_start = scanner(args.ports, args.filein, args.fileout, args.IPcol, args.OneHost, args.ports_range, args.ip_range, args.Ping)
+scan_start = scanner(args.ports, args.filein, args.fileout, args.IPcol, args.OneHost, args.ports_range, args.ip_range, args.Ping, args.DNS)
 scan_start.scan()
